24c4cd2d3facd23c529f408c175ca50799619f1d
Nikhil Gupta <nikhilking135@gmail.com>
2023-07-31 03:30:28 +0530

    changes

--- index.js
+++ index.js
@@ -31,4 +31,12 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+  })
+  
+  app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+  })
+  
 module.exports = app
